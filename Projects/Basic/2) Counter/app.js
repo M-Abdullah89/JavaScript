6330e9898e802e46ea6dae1c8f2e7b225ee3e8b4
M-Abdullah89 <166618916+M-Abdullah89@users.noreply.github.com>
2024-10-21 22:47:24 +0500

    first commit

---
+++ Projects/Basic/2) Counter/app.js
@@ -0,0 +1,23 @@
+const add = document.querySelector(".add");
+const subtract = document.querySelector(".subtract");
+const reset = document.querySelector(".reset");
+const result = document.querySelector(".result");
+
+var i = 0;
+
+add.addEventListener("click", () => {
+  i++;
+  result.textContent = i;
+});
+subtract.addEventListener("click", () => {
+  if (i == 0) {
+    result.textContent = "";
+  } else {
+    i--;
+    result.textContent = i;
+  }
+});
+
+reset.addEventListener("click", () => {
+  result.textContent = "";
+});
