ce1a7ac003070de51f0381f0dcd37c9694bf608a
M-Abdullah89 <166618916+M-Abdullah89@users.noreply.github.com>
2024-11-16 18:04:42 +0500

    first commit

---
+++ 16) Window/Window Property/1) history/index.js
@@ -0,0 +1,11 @@
+//! The history object contains the URLs visited by the user (in the browser window).
+
+//? The history object is a property of the window object.
+
+// The history object is accessed with:
+// window.history or just history:
+
+//! Properties and Methods
+
+//! Back
+//? Loads the previous URL (page) in the history list
