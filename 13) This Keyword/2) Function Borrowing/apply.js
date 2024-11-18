b87cbabe2b1008b753f378cec5481fe92d2f63eb
M-Abdullah89 <166618916+M-Abdullah89@users.noreply.github.com>
2024-11-18 18:32:51 +0500

    first commit

--- 13) This Keyword/2) Function Borrowing/apply.js
+++ 13) This Keyword/2) Function Borrowing/apply.js
@@ -1,6 +1,8 @@
 //? The apply() method is used to write methods, which can be used on different objects. It is different from the function call() because it takes arguments as an array. 
 
 //* for example
+console.log("\n Apply \n");
+console.log("\n");
 
 const person = {
     firstName: "M",
@@ -14,6 +16,12 @@ function greet(greeting) {
 const greetings = ["Hello","Hi","AoA","Hola"]
 
 // Use the apply method to invoke greet with person as the context (first arg)
-greet.apply(person, [greetings[2]]) //* AoA, M Abdullah
+greet.apply(person, [greetings[1]]) //* Hi, M Abdullah
 
 //! This process is also called function borrowing 
+
+function greet2(greeting, punctuation) {
+    console.log(greeting + ', ' + this.name + punctuation);
+  }
+  const person2 = { name: 'Abdullah' };
+  greet2.call(person2, 'Hello', '!');
