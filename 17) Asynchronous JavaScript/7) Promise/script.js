b9e18cd34664062d250228cc2166fac978368e69
M-Abdullah89 <166618916+M-Abdullah89@users.noreply.github.com>
2024-11-28 19:31:40 +0500

    first commit

---
+++ 17) Asynchronous JavaScript/7) Promise/script.js
@@ -0,0 +1,10 @@
+//? "Producing code" is code that can take some time
+
+//? "Consuming code" is code that must wait for the result
+
+//! A Promise is an Object that links Producing code and Consuming code
+
+//? A Promise is in one of three states:
+//* Pending: initial state, neither fulfilled nor rejected
+//* Fulfilled: meaning that the operation was completed successfully
+//* Rejected: meaning that the operation failed
